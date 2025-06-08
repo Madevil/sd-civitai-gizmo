52e51687da74467621f2d69ed59716e2118c100d
Madevil <760597+Madevil@users.noreply.github.com>
2025-06-08 14:50:25 +0800

    Optimized for AutoV3 support

--- scripts/gui.py
+++ scripts/gui.py
@@ -2,13 +2,7 @@ import os
 import gradio as gr
 import json
 import requests
-from modules import hashes, script_callbacks, shared
-from modules.paths_internal import models_path, data_path
-import importlib
-
-available_lora_hash_lookup = None
-lora_base = None
-lyco_base = None
+from modules import script_callbacks
 
 def request_civit_api(api_url):
     try:
@@ -38,7 +32,7 @@ def on_ui_tabs():
                 with gr.Tabs():
                     with gr.Tab(label="Hash") as _gr_tab_hash:
                         with gr.Row():
-                            _gr_tb_hash = gr.Textbox(label="Search Term:", info="supported hash algorithms: AutoV1, AutoV2, SHA256, CRC32, Blake3", interactive=True, lines=1)
+                            _gr_tb_hash = gr.Textbox(label="Search Term:", info="supported hash algorithms: AutoV1, AutoV2, AutoV3, SHA256, CRC32, Blake3", interactive=True, lines=1)
                         with gr.Row():
                             _gr_btn_hash = gr.Button(value="Search")
                     with gr.Tab(label="Version id") as _gr_tab_ver:
@@ -66,31 +60,6 @@ def on_ui_tabs():
             _version_json = ""
             _model_json = ""
             if sterm and stype in _base_url.keys():
-
-                if stype == "Hash" and len(sterm) == 12 and len(available_lora_hash_lookup) > 0:
-                    _found = None
-
-                    for _key, _val in available_lora_hash_lookup.items():
-                        if _key == sterm:
-                            _found = _val #network.NetworkOnDisk
-                            print(f"{_val.name} found via shorthash: {sterm}")
-                            break
-
-                    if _found:
-                        _title = None
-                        if _found.filename.startswith(lora_base):
-                            _title = "lora/" + os.path.relpath(os.path.splitext(_found.filename)[0], lora_base).replace("/", "\\")
-                        elif _found.filename.startswith(lyco_base):
-                            _title = "locon/" + os.path.relpath(os.path.splitext(_found.filename)[0], lyco_base).replace("/", "\\")
-                        else:
-                            print(f"cannot determine lora type from filename: {_found.filename}")
-
-                        if _title:
-                            _sha256 = hashes.sha256(_found.filename, _title, False) #don't use addnet hash
-                            if _sha256:
-                                sterm = _sha256[:10]
-                            else:
-                                print(f"failed to get sha256 from {_found.filename}")
 
                 _version_data = request_civit_api(_base_url[stype] + sterm)
                 if type(_version_data) is dict and "id" in _version_data.keys() and "modelId" in _version_data.keys():
@@ -146,32 +115,3 @@ def on_ui_tabs():
     return (_gr_block, "Gizmo", "civitai_gizmo"),
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
-
-def on_app_started(block, fastapi):
-
-    global lora_base
-    lora_base = get_lora_dir()
-
-    global lyco_base
-    lyco_base = get_locon_dir()
-
-    global available_lora_hash_lookup
-    if importlib.find_loader("lora") is not None:
-        lora = importlib.import_module("lora")
-        available_lora_hash_lookup = lora.available_lora_hash_lookup
-
-script_callbacks.on_app_started(on_app_started)
-
-def get_lora_dir():
-    lora_dir = shared.opts.data.get('civitai_folder_lora', shared.cmd_opts.lora_dir).strip()
-    if not lora_dir: lora_dir = shared.cmd_opts.lora_dir
-    return lora_dir
-
-def get_locon_dir():
-    try:
-        lyco_dir = shared.opts.data.get('civitai_folder_lyco', shared.cmd_opts.lyco_dir).strip()
-        if not lyco_dir: lyco_dir = shared.cmd_opts.lyco_dir
-        if not lyco_dir: lyco_dir = os.path.join(models_path, "LyCORIS"),
-        return lyco_dir
-    except:
-        return get_lora_dir()
